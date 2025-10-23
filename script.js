d15fc2468c9ccb41419aa7cff627be86b312c25f
komuna84 <kinayarising@gmail.com>
2025-10-22 21:50:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -104,9 +104,7 @@ class Order {
     Ui.receiptDetails(this);
     Ui.updateTotals(this);
     updatePaymentUI();
-    ();
-  }
-}
+  
 
 // ===========================================================
 // UTILITIES
